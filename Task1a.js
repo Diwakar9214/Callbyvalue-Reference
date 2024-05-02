6ba8e7905bf949ee22fb4ee829f0bc037f98d27f
Diwakar9214 <112749849+Diwakar9214@users.noreply.github.com>
2024-05-03 00:16:08 +0530

    Task1a completed

---
+++ Task1a.js
@@ -0,0 +1,28 @@
+// Print odd numbers in an array
+//  Anonymous function
+var a= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("Anonymous function");
+   var oddnum=function(array) {    
+        for(var i = 0; i < array.length; i++)
+            {   if(array[i] % 2 !==0)
+                    {
+                    
+                    console.log(array[i])
+                    }
+            }
+    }
+    
+    oddnum(a);
+
+//  IIFE
+var b= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("IIFE");
+    (function(array) {    
+        for(var i = 0; i < array.length; i++)
+            {   if(array[i] % 2 !==0)
+                    {
+                    console.log(array[i])
+                    }
+            }
+    })(b);
+
